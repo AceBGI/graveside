6dfb10f18c8b12d4d42197db5e499ab707eead01
WEI-TUNG TANG <u1169036@ad.utah.edu>
2018-02-13 12:57:19 -0700

    Add controller scripts

---
+++ Grandma Esther/Assets/scripts/Input/IInputManager.cs
@@ -0,0 +1,8 @@
+﻿public interface IInputManager
+{
+    bool IsEnabled { get; set; }
+    bool GetButton(int playerId, InputAction action);
+    bool GetButtonDown(int playerId, InputAction action);
+    bool GetButtonUp(int playerId, InputAction action);
+    float GetAxis(int playerId, InputAction action);
+}
