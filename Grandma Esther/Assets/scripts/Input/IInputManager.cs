8a26bc43741deabdceac48408894f6f4462bc4a0
AceBGI <ace-bgi@hotmail.com>
2018-02-15 09:03:37 -0700

    reloaded files from demo

--- Grandma Esther/Assets/scripts/Input/IInputManager.cs
+++
@@ -1,8 +0,0 @@
-﻿public interface IInputManager
-{
-    bool IsEnabled { get; set; }
-    bool GetButton(int playerId, InputAction action);
-    bool GetButtonDown(int playerId, InputAction action);
-    bool GetButtonUp(int playerId, InputAction action);
-    float GetAxis(int playerId, InputAction action);
-}
